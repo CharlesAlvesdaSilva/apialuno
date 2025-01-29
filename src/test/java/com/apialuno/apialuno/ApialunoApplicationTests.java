b5c32335fbe51665f76575e82ad17c3f56963e44
CharlesAlvesdaSilva <charles.silva.alves@bol.com.br>
2025-01-29 20:27:06 -0300

    feat: Projeto Senai Java

---
+++ src/test/java/com/apialuno/apialuno/ApialunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apialuno.apialuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
